80823f90c970db392ecd0f0093e7fc885c3182b2
Le Dang Ninh <dangninh010@gmail.com>
2026-07-27 02:48:12 +0700

    Register and export MailService

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,11 +3,12 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { DynamicCronQueueModule } from '../queue/dynamic-cron-queue.module';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { MailService } from './mail.service'; // Đảm bảo đã import
 
 @Module({
   imports: [PrismaModule, DynamicCronQueueModule],
   controllers: [TaskController],
-  providers: [TaskService],
-  exports: [TaskService],
+  providers: [TaskService, MailService],
+  exports: [TaskService, MailService],
 })
 export class TaskModule {}
