b4b5858b1c49defbe9a6ef0281168ccacd7191a2
Le Dang Ninh <dangninh010@gmail.com>
2026-07-27 02:49:05 +0700

    Import TaskModule and PrismaModule into CronScheduleModule

--- src/cron/cron-schedule.module.ts
+++ src/cron/cron-schedule.module.ts
@@ -4,12 +4,16 @@ import { CronScheduleController } from './cron-schedule.controller';
 import { CronScheduleService } from './cron-schedule.service';
 import { CronWorker } from './cron-worker';
 import { DYNAMIC_CRON_QUEUE } from './dynamic-cron.constants';
+import { TaskModule } from '../task/task.module';     // 1. Import thêm TaskModule
+import { PrismaModule } from '../prisma/prisma.module'; // 2. Import thêm PrismaModule
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: DYNAMIC_CRON_QUEUE,
     }),
+    TaskModule,   // 3. Thêm TaskModule vào đây (để dùng được MailService)
+    PrismaModule, // 4. Thêm PrismaModule vào đây (để dùng được PrismaService)
   ],
   controllers: [CronScheduleController],
   providers: [CronScheduleService, CronWorker],
